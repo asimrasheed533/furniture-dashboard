ac43ec315a96289a20619fbd9c210ab450a012dd
Asim Rasheed <asimrasheed442@gmail.com>
2023-06-13 20:33:49 +0500

    update

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://gym-api-production-ee00.up.railway.app/api/",
+  baseURL: "https://furniture-api-production.up.railway.app/api/",
 });
